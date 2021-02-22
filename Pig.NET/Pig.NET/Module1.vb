d2fc5830b5ab6656037a5c2c709a3d8ef93927e4
Mr. Rocket (FurryFan2003) <pawelbogusz5@gmail.com>
2021-02-22 18:14:30 +0000

    Pig.NET
    funny big in vb

---
+++ Pig.NET/Pig.NET/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.SetWindowSize(
+        Math.Min(22, Console.LargestWindowWidth),
+        Math.Min(7, Console.LargestWindowHeight))
+
+        Console.WriteLine("     __,---.__")
+        Console.WriteLine("  ,-^         ^-.__")
+        Console.WriteLine("&/           '._\ _\.")
+        Console.WriteLine("/               ''._ ")
+        Console.WriteLine("|   ,             ('')")
+        Console.WriteLine("|__,'--..--|__|---'")
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ Pig.NET/Pig.NET/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Pig.NET")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Pig.NET")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("51e18ea5-b0f7-466b-bcaf-74da49aa01a6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
