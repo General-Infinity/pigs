29d1049461890470583701b8779bf47a276941a1
Declan <ice_ice@email.com>
2021-02-23 21:37:14 +0000

    you're getting lazy for escaping these

--- pig.go
+++ pig.go
@@ -7,6 +7,6 @@ func main() {
 	fmt.Println("        ,-'         `-.__")
 	fmt.Println("      &/           `._\\ _\\")
 	fmt.Println("      /               ''._")
-	fmt.Println("      |   ,             ('')")
+	fmt.Println("      |   ,             (\")")
 	fmt.Println("      |__,'`-..--|__|--''")
 }
